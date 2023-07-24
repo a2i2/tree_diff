03979808320a5d142a42a9ce9bd7805aae28d684
Anupam Chaudhuri <anupam.chaudhuri@deakin.edu.au>
2023-07-24 12:21:58 +1000

    We updated the notebook and test to all data points

--- test_NN.py
+++ test_NN.py
@@ -3,9 +3,7 @@ from Merge_NN import *
 import cdd
 import pandas as pd
 
-coefs1, intercepts1 = load_weights("notebooks/weights_and_biases_higgs")
-coefs2, intercepts2 = load_weights("notebooks/weights_and_biases_higgs2")
-test_x, test_y = load_data('notebooks/data_higgs')
+test_x, test_y = load_data('data_higgs')
 
 #decision_space_1_region = find_region(coefs1, intercepts1, 1, point)
 #plot_decision_space_nn(decision_space_1_region,0,False)
@@ -21,10 +19,10 @@ bounded_test_y = test_y[
 # just test on a subset of the data
 bounded_test_x = bounded_test_x[:5000]
 bounded_test_y = bounded_test_y[:5000]
-#bounded_test_x = bounded_test_x[:20]
-#bounded_test_y = bounded_test_y[:20]
+#bounded_test_x = bounded_test_x[:5]
+#bounded_test_y = bounded_test_y[:5]
 
-def try_stagegy(strategy):
+def try_stagegy(strategy, coefs1, intercepts1, coefs2, intercepts2):
     pred_y = classify_on_fly_merged(
     coefs1, intercepts1, 3, coefs2, intercepts2, 3,
     strategy, bounded_test_x)
@@ -34,14 +32,21 @@ def try_stagegy(strategy):
 
     return acc, loss
 
-acc_vol, loss_vol = try_stagegy(volume_weighted_output)
-acc_global_avg, loss_global_avg = try_stagegy(avg_output)
+def compare_merge(fname):
+    coefs1, intercepts1 = load_weights(f"weights_and_biases_{fname}")
+    coefs2, intercepts2 = load_weights(f"weights_and_biases_{fname}2")
+    acc_vol, loss_vol = try_stagegy(volume_weighted_output, coefs1, intercepts1, coefs2, intercepts2)
+    acc_global_avg, loss_global_avg = try_stagegy(avg_output, coefs1, intercepts1, coefs2, intercepts2)
 
-data = {
-    'Strategy': ['Volume', 'Average'],
-    'Accuracy': [acc_vol, acc_global_avg],
-    'Loss': [loss_vol, loss_global_avg],
-}
+    data = {
+        'Strategy': ['Volume', 'Average'],
+        'Accuracy': [acc_vol, acc_global_avg],
+        'Loss': [loss_vol, loss_global_avg],
+    }
 
-df = pd.DataFrame(data)
-df.to_csv("merge_results.csv")
+    df = pd.DataFrame(data)
+    df.to_csv(f"merge_results_{fname}.csv")
+
+compare_merge("randinit_higgs")
+compare_merge("subset_higgs")
+
